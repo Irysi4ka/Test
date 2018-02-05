3b75145099faa1c0ac9470b9e76e8b908eaf159f
Irysi4ka <36077989+Irysi4ka@users.noreply.github.com>
2018-02-05 16:07:36 +0300

    Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import application.beans.Purchase;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Purchase purchase = new Purchase();
+    }
+}
---
+++ src/application/beans/Purchase.java
@@ -0,0 +1,14 @@
+package application.beans;
+
+public class Purchase {
+
+    private String nameOfProduct = "Ноутбук";
+    private int price;
+    private int discount = 19;
+    private double purchaseAmount;
+    private int numberOfProduct = 1;
+    private boolean paidWithCash = true;
+
+
+
+}
