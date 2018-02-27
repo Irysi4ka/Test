4397c445db0f86fb940d4981a1501cbf4d9fa674
Irysi4ka <36077989+Irysi4ka@users.noreply.github.com>
2018-02-27 14:51:17 +0300

    1

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,23 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Purchase purchase = new Purchase("Ноутбук", 425,
-                19, 2, true);
-        Purchase purchase2 = new Purchase("Сумка", 30,
+        Purchase purchase = new Purchase("Сумка", 425,
+                19, 2, false);
+        Purchase purchase2 = new Purchase("Чехол", 30,
                 15, 1, true);
-
-        System.out.println(purchase.toString());
-        System.out.println(purchase2.toString());
+        Purchase purchase3 = new Purchase("Телефон", 900,
+                15, 1, true);
+        Purchase purchase4 = new Purchase("Ноутбук", 480,
+                15, 3, false);
 
         Client client = new Client("Ирина", "Игнатенко",
                 "Игоревна", 2361.2, 234);
         Client client2 = new Client("Иван", "Иванов",
-                "Иванович", 1000, 700);
+                "Иванович", 1000, 400);
+        Client client3 = new Client("Андрей", "Михайлович",
+                "Кондрачук", 4000, 900);
+        Client client4 = new Client("Анна", "Владимировна",
+                "Семакова", 2000, 1200);
 
+        System.out.println("\n" + purchase.toString());
         System.out.println(client.toString());
-        System.out.println(client2.toString());
         client.buy(purchase);
+
+        System.out.println("\n" + purchase2.toString());
+        System.out.println(client2.toString());
         client2.buy(purchase2);
+
+        System.out.println("\n" + purchase3.toString());
+        System.out.println(client3.toString());
+        client3.buy(purchase3);
+
+        System.out.println("\n" + purchase4.toString());
+        System.out.println(client4.toString());
+        client4.buy(purchase4);
 
     }
 }
