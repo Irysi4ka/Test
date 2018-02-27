de730a7aace0a71b1b0ce3fdab9dd83b24e08b09
Irysi4ka <36077989+Irysi4ka@users.noreply.github.com>
2018-02-27 13:57:27 +0300

    1

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,26 +9,21 @@ public class Main {
 
         Purchase purchase = new Purchase("Ноутбук", 425,
                 19, 2, true);
-
-        System.out.println(purchase.toString());
-
-        Client client = new Client("Ирина", "Игнатенко",
-                "Игоревна", 2361.2, 234);
-
-        System.out.println(client.toString());
-        client.buy(purchase);
-
         Purchase purchase2 = new Purchase("Сумка", 30, 15,
                 1, true);
 
+        System.out.println(purchase.toString());
         System.out.println(purchase2.toString());
 
+        Client client = new Client("Ирина", "Игнатенко",
+                "Игоревна", 2361.2, 234);
         Client client2 = new Client("Иван", "Иванов",
                 "Иванович", 1000, 700);
 
+        System.out.println(client.toString());
         System.out.println(client2.toString());
+        client.buy(purchase);
         client2.buy(purchase);
-
 
     }
 }
